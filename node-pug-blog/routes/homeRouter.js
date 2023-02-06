7ac36c72193286080cb73ce8f19ba3378ca8b08d
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-02-06 16:08:11 -0500

    huh

--- node-pug-blog/routes/homeRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const Posts = require('../modules/postsSchema')
-
-router.get('/', async(req, res, next) => {
-	const posts = await Posts.find({})
-	res.render('index', { posts: posts})
-})
-
-module.exports = router;
